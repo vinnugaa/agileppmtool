4322b2fef261297a93996aa9ef794c00bb66f641
vinnugaa <vini.vini.2@gmail.com>
2020-03-09 02:06:22 +0000

    project

---
+++ ppmproject/src/main/java/com/vinay/demo/PpmApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmApplication.class, args);
+    }
+
+}
---
+++ ppmproject/src/test/java/com/vinay/demo/PpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
